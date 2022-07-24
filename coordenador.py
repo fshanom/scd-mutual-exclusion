af7db8f94c1690957b98adb84b85554c81f48e92
fshanom <filipeshanom@gmail.com>
2022-07-24 16:03:39 -0300

    Update files

--- coordenador.py
+++ coordenador.py
@@ -8,7 +8,7 @@
     # Incluindo: INSTANTE DA MENSAGEM | TIPO DA MENSAGEM | PROCESSO ORIGEM OU DESTINO
 
 # Comunicação entre processos usando sockets
-# Cada processo tem seu próprio socket, e o coordenador vai ter uma estrutura de dados com esses sockets. 
+# Cada processo tem seu próprio socket, e o coordenador vai ter uma estrutura de dados com esses sockets.
 # Usar sockets tipo UDP (Nesse caso o coordenador tem um só socket para todos os processos).
 
 # A thread de interface deve ficar bloqueada, aguardando os comandos do terminal:
@@ -17,3 +17,166 @@
     # 3) Encerrar a execução
 # As duas threads (interface e a de algoritmo) acessam a mesma fila, então devem ser sincronizadas.
 
+from asyncio import Queue
+from datetime import datetime
+import json
+import logging
+import socket
+from threading import Thread
+import threading
+import time
+import util
+
+semaphore = threading.Semaphore()
+
+#Arquivo de log
+#logging.basicConfig(filename='resultado.txt', filemode='w', level=logging.INFO)
+
+#Carrega as configurações do server
+with open("config.json", "r") as configFile:
+    config = json.load(configFile)
+
+#Variáveis globais
+BUFFER_SIZE = config["buffer_size"]
+DELIMITER = config["message_delimiter"]
+MSG_TERMINATOR = config["message_terminator"]
+DELAY = config["delay"]
+REQ = "REQUEST"
+GRT = "GRANT"
+REL = "RELEASE"
+
+#nÃO SEI PRA QUE SERVE
+thread_pool = []
+lock = threading.Lock()
+
+fila = util.Queue()
+
+class ThreadCoordenador(Thread):
+    #sobe socket do coordenador na porta port
+    def __init__(self, socket, ip, port):
+        Thread.__init__(self)
+        self.socket = socket
+        self.process_id = 0
+        self.ip = ip
+        self.port = port
+        self.send_msg_queue = util.Queue()
+
+    #método que escreve na rc
+    def run(self):
+        #logging.info("Conectado a " + self.ip + ":" + str(self.port))
+        data = ""
+        #self.console()
+        while True:
+            #pega a mensagem do processo
+            data = str(data) + str(self.socket.recv(BUFFER_SIZE))
+            if data:
+                while MSG_TERMINATOR in data:
+                    pos = data.find(MSG_TERMINATOR)
+                    msg = data[:pos]
+                    data = data[pos+1:]
+
+                    #abrir o arquivo de log em modo append
+                    #obtem a hora atual
+                    datahora = msg.split(DELIMITER)[0]
+                    print(datahora)
+
+                    #escreve o id e a hora atual no final do arquivo
+
+                    if "PID" in msg:
+                        self.process_id = msg.split(":")[1]
+                    if REQ in msg or REL in msg:
+                        self.send_msg_queue.push(msg)
+
+                        log = open("resultado.txt", "a")
+                        now = datetime.utcnow()
+                        current_time = now.strftime("%H:%M:%S.%f")
+                        data =  data.replace("'", "")
+                        log.write(current_time + " | GRANT | Processo " + data + " | Teste \n")
+                        log.close()
+                        fila.push(data)
+                        self.escreveRC(fila)
+
+                    if GRT in msg:
+                        with lock:
+                            time.sleep(DELAY)
+                            self.forward_reply_message(msg)
+
+    def escreveRC(self, pros):
+
+        #for thread in thread_pool:
+            #if int(self.process_id) != int(thread.process_id):
+        while not pros.isEmpty():
+
+                message = pros.pop()
+                #print(message + " está mandando mensagem")
+                now = datetime.utcnow()
+                current_time = now.strftime("%H:%M:%S.%f")
+                semaphore.acquire()
+                #thread.socket.send(message + MSG_TERMINATOR)
+                log = open("resultado.txt", "a")
+                now = datetime.utcnow()
+                current_time = now.strftime("%H:%M:%S.%f")
+                log.write(current_time + " | RELEASE | Processo " + message + " | Teste \n")
+                log.close()
+                semaphore.release()
+
+    def forward_reply_message(self, message):
+        pid = int(message.split(DELIMITER)[1])
+        #print("Send  msg: " + message + "to " + str(pid))
+        for thread in thread_pool:
+            #print(thread.process_id + " " + str(pid))
+            if int(thread.process_id) == pid:
+                semaphore.acquire()
+                time.sleep(DELAY)
+                thread.socket.send(message + MSG_TERMINATOR)
+                time.sleep(DELAY)
+                semaphore.release()
+    
+class ThreadConsole(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+    
+    def run(self):
+        while True:
+            print("---------- MENU ----------\n")
+            print("1) Imprimir lista de pedidos atual\n")
+            print("2) imprimir quantas vezes cada processo foi atendido\n")
+            print("3) Encerrar a execução\n")
+            print("Digite a opção desejada:")
+            op = input()
+            #print(op)
+            
+            if(op == '1'):
+                '''
+                texto = open("resultado.txt", "r")
+                lines = texto.readlines()
+                for line in lines:
+                    print(line)
+                '''
+                print(fila)
+            if(op == '3'):
+                print("Fim da execução")
+                break
+                #self.is_alive = False
+
+
+if __name__ == "__main__":
+    host_IP = config["IP"]
+    host_port = config["port"]
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host_IP, host_port))
+    
+    #Thread do console
+    threadConsole = ThreadConsole()
+    threadConsole.start()
+
+    while True:
+        server_socket.listen(5)
+        (connection_socket, (ip, port)) = server_socket.accept()
+
+        #Thread do coordenador
+        threadCoordenador = ThreadCoordenador(connection_socket, ip, port)
+        threadCoordenador.start()
+
+        thread_pool.append(threadCoordenador)
--- processo.py
+++ processo.py
@@ -11,3 +11,116 @@
 # Uma execução com n processos e r repetições deve dar origem a um arquivo de log com n*r linhas
 # Deve ser verificado se o arquivo tem esse número de linhas e se as linhas estão corretas respeitando o relógio, e se cada processo escreveu r vezes.
 # Verificar os GRANT e RELEASE, apóis um grant sempre tem release. A ordem dos processos das mensagens REQUEST deve ser a mesma que a ordem dos processos da mensagem Release.
+
+
+
+from datetime import datetime
+import logging
+import socket
+import time
+from asyncio import Queue
+import json
+import argparse
+
+#Carrega as configurações do server
+with open("config.json", "r") as configFile:
+    config = json.load(configFile)
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#serv.connect(config["IP"],config["port"])
+
+#Log da RC
+logging.basicConfig(filename='resultado.txt', filemode='w', level=logging.INFO)
+
+class Processo:
+    def __init__(self, processo_id, num_processo):
+        self.id = processo_id
+        self.connections = []
+        self.number_set = []
+        self.processo_count = num_processo
+        self.number_set_idx = 0
+        #self.get_number_set()
+        print("Iniciando Processo "+ str(self.id))
+    
+    #Método que solicita o acesso para escrita na RC
+    def request(self):
+        #while len(self.number_set) > self.number_set_idx:
+
+        log = open("resultado.txt", "a")
+        now = datetime.utcnow()
+        current_time = now.strftime("%H:%M:%S.%f")
+        log.write(current_time + " | REQUEST | Processo " + str(self.id) + " | Teste \n")
+        log.close()
+        #logging.info( current_time + " | REQUEST | Processo " + str(self.id) + " | Teste")
+
+        # lamport
+        # 1 envia pedido com timestamp para todos os processos incluindo a si mesmo
+        # 2 aguarda confirmação de todos os processos
+        # 3 se o pedido estiver na cabeça da fila e todas as confirmações chegarem, entra na RC
+
+
+        connection = self.__connect(True)
+        msg = current_time + ' | REQUEST #' + str(self.id)
+        connection.sendall(msg.encode())
+        print('Processo ' + str(self.id) + ' mandou solicitação de escrita Coordenador')
+        self.connections.append(connection)
+        self.number_set_idx = self.number_set_idx + 1
+        time.sleep(3)    
+    
+    
+    def get_number_set(self):
+        num_file = open(config["log_file"], 'r')
+        lines = num_file.readlines()
+        for i in range(0, len(lines)):
+            if i % self.processo_count == self.id:
+                self.number_set.append(int(lines[i]))
+        num_file.close()
+
+    #Método que conecta no socket do coordenador
+    def __connect(self, retry):
+        try:
+            to_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            to_socket.connect((config["IP"],config["port"]))
+            print("Processo " + str(self.id) + " Conectado")
+        except socket.error:
+            if retry:
+                print("Conexão falhou no processo " + str(self.id))
+                time.sleep(2)
+                return self.__connect(self.assigned_server, False)
+            else:
+                print("Processo " + str(self.id) + " deu Erro")
+        finally:
+            return to_socket
+
+    def close(self):
+        self.connection.close()
+
+def main():
+    #open('cs.txt', 'w').close()
+    arguments = get_parser().parse_args()
+    p_counts = arguments.num_processos
+    clients = []
+    cid = 1
+
+    for p in range(p_counts):
+        pross = Processo(cid,p_counts)
+        clients.append(pross)
+        cid = cid + 1
+
+    for c in clients:
+        try:
+            c.request()
+            time.sleep(5)
+        except Exception as e:
+            print(str(e))
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Processos')
+    parser.add_argument('num_processos', type=int)
+
+    return parser
+
+
+if __name__ == "__main__":
+    main()
